00e196d2fd3b70aff4c7342f81d16df50e43c364
narutiga <100295602+narutiga@users.noreply.github.com>
2023-03-25 22:14:12 +0900

    ✨ add useQueryTodos hook.

---
+++ src/lib/tanstackQuery/useQueryTodos.ts
@@ -0,0 +1,30 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+type Todo = {
+  id: string;
+  title: string;
+  isDone: boolean;
+  dueDate: string;
+};
+
+export const useQueryTodos = (dueDate: string) => {
+  const getTodos = async () => {
+    const response = await axios.get(
+      `http://localhost:8080/api/v1/todos/?dueDate=${dueDate}`,
+      {
+        withCredentials: true,
+      }
+    );
+    if (!response.data) {
+      throw new Error("No data found");
+    } else {
+      return response.data;
+    }
+  };
+  return useQuery<Todo[], Error>({
+    queryKey: [`${dueDate}todos`],
+    queryFn: getTodos,
+    staleTime: Infinity,
+  });
+};
